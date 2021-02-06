55198b7f8bbe3efbb2261dbde257479c45b7709c
murodbeksaidov <murodbeksaidov9999@gmail.com>
2021-02-06 18:38:07 +0500

    Islombek Yadgarov

--- app/src/main/java/com/ervinod/googlemaproute/MainActivity.java
+++ app/src/main/java/com/ervinod/googlemaproute/MainActivity.java
@@ -1,277 +1,236 @@
 package com.ervinod.googlemaproute;
-
-import android.Manifest;
-import android.app.Activity;
-import android.app.Dialog;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Point;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
+import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import androidx.annotation.DrawableRes;
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.FragmentActivity;
-
-import com.ahmadrosid.lib.drawroutemap.DrawMarker;
-import com.ahmadrosid.lib.drawroutemap.DrawRouteMaps;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.LatLngBounds;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-public class MainActivity extends FragmentActivity implements LocationListener, OnMapReadyCallback {
-
-    GoogleMap mGoogleMap;
-
-    double mLatitude = 0.0;
-    double mLongitude = 0.0;
-
-    double destinationLatitude = 0.0;
-    double destinationLongitude = 0.0;
-    Marker myMarker = null;
-
+import com.google.android.gms.maps.model.PolylineOptions;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Objects;
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+    ArrayList<LatLng> markerPoints= new ArrayList<LatLng>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        if (getIntent().getExtras() != null) {
-            String latitude = getIntent().getStringExtra("LAT");
-            String longitude = getIntent().getStringExtra("LNG");
-            if (latitude != null) {
-                destinationLatitude = Double.parseDouble(latitude);
-            }
-
-            if (longitude != null) {
-                destinationLongitude = Double.parseDouble(longitude);
-            }
-        } else {
-            destinationLatitude = 20.902901;
-            destinationLongitude = 74.775139;
-        }
-
-
-        // Getting Google Play availability status
-        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getBaseContext());
-
-        if (status != ConnectionResult.SUCCESS) { // Google Play Services are not available
-
-            int requestCode = 10;
-            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, this, requestCode);
-            dialog.show();
-
-        } else { // Google Play Services are available
-
-            // Getting reference to the SupportMapFragment
-            SupportMapFragment fragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-
-            // Getting Google Map
-            fragment.getMapAsync(this);
-
-        }
-
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        assert mapFragment != null;
+        mapFragment.getMapAsync(this);
     }
-
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mGoogleMap = googleMap;
-
-        listenLocation();
-        //  mService.requestLocationUpdates();
-        // for Map tool disable
-        //mGoogleMap.getUiSettings().setMapToolbarEnabled(false);
-
-        // for Zoom Button Enable on Google Map
-        //mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
-
-        //for Location  Button enable on Google Map
-        //mGoogleMap.getUiSettings().setMyLocationButtonEnabled(true);
-
-  /*      //add custom marker to your location
-        mGoogleMap.addMarker(new MarkerOptions().position(latLng).
-                icon(BitmapDescriptorFactory.fromBitmap(
-                        createCustomMarker(LocationRouteActivity.this, R.drawable.ic_my_location, "Vinod")))).setTitle("Your Location");
-
-        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-        mGoogleMap.animateCamera(CameraUpdateFactory.zoomTo(12));
-*/
-
-        /*LatLng origin = new LatLng(20.904221, 74.774895);
-        LatLng destination = new LatLng(20.902901, 74.775139);
-
-        DrawRouteMaps.getInstance(LocationRouteActivity.this, GOOGLE_API_KEY)
-                .draw(origin, destination, mGoogleMap);
-        DrawMarker.getInstance(this).draw(mGoogleMap, origin, R.drawable.ic_source, "Your Location");
-        DrawMarker.getInstance(this).draw(mGoogleMap, destination, R.drawable.ic_destination, "Destination Location");
-
-        LatLngBounds bounds = new LatLngBounds.Builder()
-                .include(origin)
-                .include(destination).build();
-        Point displaySize = new Point();
-        getWindowManager().getDefaultDisplay().getSize(displaySize);
-        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, displaySize.x, 350, 30));*/
-
-        LatLng origin = new LatLng(20.904221, 74.774895);
-        //LatLng destination = new LatLng(20.902901, 74.775139);
-        LatLng destination = new LatLng(destinationLatitude, destinationLongitude);
-        plotLocation(origin, destination);
-
-
-        if (Build.VERSION.SDK_INT >= 23 && (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
-                ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
-            showPermissionDialog();
-            return;
-        }
-
-        mGoogleMap.setMyLocationEnabled(true);
-
-
-        // Getting LocationManager object from System Service LOCATION_SERVICE
-        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
-        // Creating a criteria object to retrieve provider
-        Criteria criteria = new Criteria();
-
-        // Getting the name of the best provider
-        String provider = locationManager.getBestProvider(criteria, true);
-
-        // Getting Current Location From GPS
-        Location location = locationManager.getLastKnownLocation(provider);
-
-        if (location != null) {
-            //onLocationChanged(location);
-        }
-
-        locationManager.requestLocationUpdates(provider, 5000, 10, this);
-
-    }
-
-
-
-    public void updateMarkerLocation(final LatLng updatedLatLng) {
-
-        runOnUiThread(new Runnable() {
+        mMap = googleMap;
+        LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 16));
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
             @Override
-            public void run() {
-                LatLng latLng = new LatLng(mLatitude, mLongitude);
-
-                if (myMarker == null) {
-                    MarkerOptions markerOptions = new MarkerOptions().position(updatedLatLng).icon(BitmapDescriptorFactory.fromBitmap(
-                            createCustomMarker(MainActivity.this,
-                                    R.drawable.ic_my_location, "Your Location")));
-
-                    myMarker = mGoogleMap.addMarker(markerOptions);
-
-                } else {
-                    myMarker.setPosition(updatedLatLng);
+            public void onMapClick(LatLng latLng) {
+                if (markerPoints.size() > 1) {
+                    markerPoints.clear();
+                    mMap.clear();
+                }
+                // Adding new item to the ArrayList
+                markerPoints.add(latLng);
+                // Creating MarkerOptions
+                MarkerOptions options = new MarkerOptions();
+                // Setting the position of the marker
+                options.position(latLng);
+                if (markerPoints.size() == 1) {
+                    options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+                } else if (markerPoints.size() == 2) {
+                    options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
+                }
+                // Add new marker to the Google Map Android API V2
+                mMap.addMarker(options);
+                // Checks, whether start and end locations are captured
+                if (markerPoints.size() >= 2) {
+                    LatLng origin = markerPoints.get(0);
+                    LatLng dest = markerPoints.get(1);
+                    // Getting URL to the Google Directions API
+                    String url = getDirectionsUrl(origin, dest);
+                    DownloadTask downloadTask = new DownloadTask();
+                    // Start downloading json data from Google Directions API
+                    downloadTask.execute(url);
                 }
             }
         });
-
     }
+    private class DownloadTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... url) {
 
-    public void plotLocation(LatLng origin, LatLng destination) {
-        if (mGoogleMap != null) {
-            mGoogleMap.clear();
+            String data = "";
 
-            //LatLng origin = new LatLng(20.904221, 74.774895);
-            //LatLng destination = new LatLng(20.902901, 74.775139);
+            try {
+                data = downloadUrl(url[0]);
+            } catch (Exception e) {
+                Log.d("Background Task", e.toString());
+            }
+            return data;
+        }
 
-            DrawRouteMaps.getInstance(MainActivity.this, getResources().getString(R.string.google_map_api_key))
-                    .draw(origin, destination, mGoogleMap);
-            DrawMarker.getInstance(this).draw(mGoogleMap, origin, R.drawable.ic_source, "Your Location");
-            DrawMarker.getInstance(this).draw(mGoogleMap, destination, R.drawable.ic_destination, "Destination Location");
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
 
-            LatLngBounds bounds = new LatLngBounds.Builder()
-                    .include(origin)
-                    .include(destination).build();
-            Point displaySize = new Point();
-            getWindowManager().getDefaultDisplay().getSize(displaySize);
+            ParserTask parserTask = new ParserTask();
 
-            mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, displaySize.x, 350, 30));
 
+            parserTask.execute(result);
 
         }
     }
 
-    //creating custom icon for marker to be shown on map
-    public static Bitmap createCustomMarker(Context context, @DrawableRes int resource, String _name) {
 
-        View marker = ((LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.custom_marker_layout, null);
+    /**
+     * A class to parse the Google Places in JSON format
+     */
+    private class ParserTask extends AsyncTask<String, Integer, List<List<HashMap<String, String>>>> {
 
-        ImageView markerImage = (ImageView) marker.findViewById(R.id.user_marker);
-        markerImage.setImageResource(resource);
-        TextView txt_name = (TextView) marker.findViewById(R.id.title);
-        txt_name.setText(_name);
+        // Parsing the data in non-ui thread
+        @Override
+        protected List<List<HashMap<String, String>>> doInBackground(String... jsonData) {
 
-        DisplayMetrics displayMetrics = new DisplayMetrics();
-        ((Activity) context).getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
-        marker.setLayoutParams(new ViewGroup.LayoutParams(52, ViewGroup.LayoutParams.WRAP_CONTENT));
-        marker.measure(displayMetrics.widthPixels, displayMetrics.heightPixels);
-        marker.layout(0, 0, displayMetrics.widthPixels, displayMetrics.heightPixels);
-        marker.buildDrawingCache();
-        Bitmap bitmap = Bitmap.createBitmap(marker.getMeasuredWidth(), marker.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(bitmap);
-        marker.draw(canvas);
+            JSONObject jObject;
+            List<List<HashMap<String, String>>> routes = null;
 
-        return bitmap;
-    }
+            try {
+                jObject = new JSONObject(jsonData[0]);
+                DirectionsJSONParser parser = new DirectionsJSONParser();
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
+                routes = parser.parse(jObject);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return routes;
+        }
 
-    public void listenLocation() {
-        if (!ServiceUtils.isMyServiceRunning(LocationForegroundService.class, this)) {
-            Intent startIntent = new Intent(this, LocationForegroundService.class);
-            startIntent.setAction("STARTFOREGROUND_ACTION");
-            startService(startIntent);
+        @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+        @Override
+        protected void onPostExecute(List<List<HashMap<String, String>>> result) {
+            ArrayList<LatLng> points = null;
+            PolylineOptions lineOptions = null;
+            MarkerOptions markerOptions = new MarkerOptions();
 
-        } else {
-            Log.d("Service", "Already Running");
+            for (int i = 0; i < result.size(); i++) {
+                points = new ArrayList<>();
+                lineOptions = new PolylineOptions();
+
+                List<HashMap<String, String>> path = result.get(i);
+
+                for (int j = 0; j < path.size(); j++) {
+                    HashMap<String, String> point = path.get(j);
+
+                    double lat = Double.parseDouble(Objects.requireNonNull(point.get("lat")));
+                    double lng = Double.parseDouble(Objects.requireNonNull(point.get("lng")));
+                    LatLng position = new LatLng(lat, lng);
+
+                    points.add(position);
+                }
+
+                lineOptions.addAll(points);
+                lineOptions.width(12);
+                lineOptions.color(Color.RED);
+                lineOptions.geodesic(true);
+
+            }
+
+// Drawing polyline in the Google Map for the i-th route
+            mMap.addPolyline(lineOptions);
         }
     }
 
-    @Override
-    public void onLocationChanged(@NonNull Location location) {
-        mLatitude = location.getLatitude();
-        mLongitude = location.getLongitude();
-        LatLng sourceLatLng = new LatLng(mLatitude, mLongitude);
-        LatLng destinationLatLng = new LatLng(destinationLatitude, destinationLongitude);
-        //updateMarkerLocation(sourceLatLng);
-        plotLocation(sourceLatLng, destinationLatLng);
+    private String getDirectionsUrl(LatLng origin, LatLng dest) {
+
+        // Origin of route
+        String str_origin = "origin=" + origin.latitude + "," + origin.longitude;
+
+        // Destination of route
+        String str_dest = "destination=" + dest.latitude + "," + dest.longitude;
+
+        // Sensor enabled
+        String sensor = "sensor=false";
+        String mode = "mode=driving";
+        // Building the parameters to the web service
+        String parameters = str_origin + "&" + str_dest + "&" + sensor + "&" + mode;
+
+        // Output format
+        String output = "json";
+
+        // Building the url to the web service
+        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters;
+
+
+        return url;
     }
 
-    public void showPermissionDialog() {
-        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+    /**
+     * A method to download json data from url
+     */
+    private String downloadUrl(String strUrl) throws IOException {
+        String data = "";
+        InputStream iStream = null;
+        HttpURLConnection urlConnection = null;
+        try {
+            URL url = new URL(strUrl);
 
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            urlConnection.connect();
+
+            iStream = urlConnection.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(iStream));
+
+            StringBuffer sb = new StringBuffer();
+
+            String line = "";
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+
+            data = sb.toString();
+
+            br.close();
+
+        } catch (Exception e) {
+            Log.d("Exception", e.toString());
+        } finally {
+            assert iStream != null;
+            iStream.close();
+            urlConnection.disconnect();
+        }
+        return data;
     }
-
 }
